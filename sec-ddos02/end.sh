e0539afe26448fa1d411e72bbdd8a00d564c001f
FlavioLionelRita <flaviolrital@hotmail.com>
2023-07-16 03:42:31 +0200

    configure nginx container

---
+++ sec-ddos02/end.sh
@@ -0,0 +1,10 @@
+# remove tables
+docker exec sec-ddos_orm_1 lambdaorm drop -w ./workspace
+# stop database and service
+docker-compose -p sec-ddos down --remove-orphans
+# remove image
+docker rmi sec-ddos_nginx
+# remove volumes
+docker volume rm sec-ddos_grafana_data
+docker volume rm sec-ddos_prometheus_data
+docker volume rm sec-ddos_postgres_data
