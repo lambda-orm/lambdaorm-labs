57662ab16dc1f86a1a528849e60c97b5e5164673
FlavioLionelRita <flaviolrital@hotmail.com>
2023-11-10 03:15:49 +0100

    ci(release): release 0.0.5
        #0

--- labs/issue-179/src/index.ts
+++ labs/issue-179/src/index.ts
@@ -3,7 +3,9 @@ import { Orders } from './northwind/domain'
 ( async ()=> {
 	try {		
 		await orm.init('lambdaORM.yaml')
-		const query = (ordNo: number) => Orders.details.filter(o => o.orderId === ordNo).map(o => ({ ordNo:o.orderId, total: avg(o.unitPrice * o.quantity )}))
+		const query = (ordNo: number) => Orders.details
+											.filter(o => o.orderId === ordNo)
+											.map(o => ({ ordNo:o.orderId, total: avg(o.unitPrice * o.quantity )}))
 		const result = await orm.execute(query, { ordNo: 5 }, {stage:'MySQL'});
         console.log(JSON.stringify(result))		
 	} catch (error:any) {
